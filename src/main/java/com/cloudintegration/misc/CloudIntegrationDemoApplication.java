d1edbaa2a4db5d35799ee1003a595e7e922ecb73
IamVk009 <imvk009@gmail.com>
2025-11-01 10:57:02 +0530

    Initial Setup

---
+++ src/main/java/com/cloudintegration/misc/CloudIntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cloudintegration.misc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudIntegrationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudintegration/misc/CloudIntegrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudintegration.misc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudIntegrationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
